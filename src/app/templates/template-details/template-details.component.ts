bd5c6a0aeaa7de97316691a437fd8de77567d93d
Jon foster <redcabinny@gmail.com>
2017-06-25 17:03:39 -0400

    updated template details

--- src/app/templates/template-details/template-details.component.ts
+++ src/app/templates/template-details/template-details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TemplatesService } from '../../services/templates.service';
+import { Template } from '../../models/template.model';
+import { Property } from '../../models/property.model';
+import { Http } from '@angular/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-template-details',
@@ -8,14 +12,33 @@ import { TemplatesService } from '../../services/templates.service';
 })
 export class TemplateDetailsComponent implements OnInit {
 
-  constructor(private templatesService: TemplatesService) { }
+  template;
+
+  constructor(private templatesService: TemplatesService,
+              private http: Http,
+              private route: ActivatedRoute
+  			  ) { }
 
   ngOnInit() {
-  	this.templatesService.emitTemplateIndex.subscribe(
-  		(data) => {
-  			console.log(data, "in service")
-  		}
-  		)
+  	// this.templatesService.emitTemplateIndex.subscribe(
+  	// 	(data) => {
+  	// 		this.http.get("http://localhost:3000/templates/" + data)
+  	// 			.subscribe((data) => {
+  	// 				var obj = JSON.parse(data["_body"]);
+  	// 				var template = this.templatesService.buildTemplate(obj);
+  	// 				this.template = template;
+  	// 			})
+  	// 		}
+  	// 	)
+  	this.route.params 
+  		.subscribe((data) => {
+  			this.http.get("http://localhost:3000/templates/" + data["id"])
+  				.subscribe((data) => {
+  					var obj = JSON.parse(data["_body"]);
+  					var template = this.templatesService.buildTemplate(obj);
+  					this.template = template;
+  				})
+  		})
   }
 
 }
