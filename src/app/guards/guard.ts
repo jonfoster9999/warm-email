d8d7950a588d6a3d1bfff690b1dfe261c3a60c17
Jon foster <redcabinny@gmail.com>
2017-07-02 09:58:55 -0400

    updated guard.ts

---
+++ src/app/guards/guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
+import { Observable } from 'rxjs/observable'
+
+@Injectable()
+export class CanActivateViaUserService implements CanActivate {
+
+	constructor(private usersService: UsersService, private router: Router) {}
+
+	canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean { 
+		if (this.usersService.loggedIn) {
+			return true;
+		} else {
+			console.log("Refused")
+			this.router.navigate(['/login'])
+		}
+	}
+}
