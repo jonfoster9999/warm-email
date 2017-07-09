8b57827e930d76a6857d2c4a77ce92b5849e8781
Jon foster <redcabinny@gmail.com>
2017-07-09 13:02:34 -0400

    updated template filter pipe

---
+++ src/app/pipes/templatefilter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'templatefilter'
+})
+export class TemplatefilterPipe implements PipeTransform {
+
+  transform(controls: any, body?: any): any {
+    var bodyArray = body.match(/\S+/g).filter((element) => {
+    	return element.includes("**");
+    }).map( (el) => {
+      var el = el.replace(/[**]/gi, "")
+    	return el.match(/[A-Za-z0-9_]+/gi)[0]
+    })
+    return controls.filter((control) => {
+    	return bodyArray.includes(control["name"])
+    })
+  }
+}
