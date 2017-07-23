bb368dc2adf3b3e6d9ca199b33da17c1d2f2498d
Jon foster <redcabinny@gmail.com>
2017-07-23 11:58:31 -0400

    removed test 6

--- src/app/pipes/templatefilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TemplatefilterPipe } from './templatefilter.pipe';
-
-describe('TemplatefilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TemplatefilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
